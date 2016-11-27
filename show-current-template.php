093cb350ffae9f3a133fc66523a782c28e9bd15d
JOTAKI, Taisuke <tekapo@gmail.com>
2016-11-27 13:20:08 +0900

    cleanup code

--- show-current-template.php
+++ show-current-template.php
@@ -38,8 +38,8 @@ new Show_Template_File_Name();
 class Show_Template_File_Name {
 
 	function __construct() {
-		add_action( "admin_bar_menu", array( &$this, "show_template_file_name_on_top" ), 9999 );
-		add_action( 'wp_enqueue_scripts', array( &$this, "add_current_template_stylesheet" ), 9999 );
+		add_action( 'admin_bar_menu', array( &$this, 'show_template_file_name_on_top' ), 9999 );
+		add_action( 'wp_enqueue_scripts', array( &$this, 'add_current_template_stylesheet' ), 9999 );
 	}
 
 	public function show_template_file_name_on_top( $wp_admin_bar ) {
@@ -62,7 +62,7 @@ class Show_Template_File_Name {
 					. $current_theme_name;
 			$parent_theme_name	 = $current_theme->parent()->Name;
 			$parent_theme_name	 = ' (' . $parent_theme_name
-					. __( "'s child", 'show-current-template' ) . ")";
+					. __( "'s child", 'show-current-template' ) . ')';
 			$parent_or_child	 = $child_theme_name . $parent_theme_name;
 		} else {
 			$parent_or_child = __( 'Theme name: ', 'show-current-template' )
@@ -129,7 +129,7 @@ class Show_Template_File_Name {
 		} else {
 			$is_older_than_3_8 = '-old';
 		}
-		
+
 		$stylesheet_path = plugins_url( 'css/style' . $is_older_than_3_8 . '.css', __FILE__ );
 		wp_register_style( 'current-template-style', $stylesheet_path );
 		wp_enqueue_style( 'current-template-style' );
