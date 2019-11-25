1809ad6e3c54651657abb1089c35e80c3f1cad4e
JOTAKI, Taisuke <tekapo@gmail.com>
2019-11-25 14:22:03 +0900

    sync with svn

--- show-current-template.php
+++ show-current-template.php
@@ -1,63 +1,47 @@
 <?php
-/**
-Plugin Name: Show Current Template
-Plugin URI: http://wp.tekapo.com/
-Description: Show the current template file name in the tool bar.
-Author: JOTAKI Taisuke
-Version: 0.3.0
-Author URI: http://tekapo.com/
-Text Domain: show-current-template
-Domain Path: /languages/
 
-@package Show Current Template
+/*
+  Plugin Name: Show Current Template
+  Plugin URI: http://wp.tekapo.com/
+  Description: Show the current template file name in the tool bar.
+  Author: JOTAKI Taisuke
+  Version: 0.3.0
+  Author URI: http://tekapo.com/
+  Text Domain: show-current-template
+  Domain Path: /languages/
 
-License:
-Released under the GPL license
-http://www.gnu.org/copyleft/gpl.html
+  License:
+  Released under the GPL license
+  http://www.gnu.org/copyleft/gpl.html
 
-Copyright 2013 (email : tekapo@gmail.com)
+  Copyright 2013 (email : tekapo@gmail.com)
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 load_plugin_textdomain( 'show-current-template', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
 new Show_Template_File_Name();
 
-/**
- * Class.
- */
 class Show_Template_File_Name {
 
-	/**
-	 * Construct.
-	 */
 	function __construct() {
-		// Construction.
-		add_action( 'admin_bar_menu', array( &$this, 'show_template_file_name_on_top' ), 9999 );
-		add_action( 'wp_enqueue_scripts', array( &$this, 'add_current_template_stylesheet' ), 9999 );
+		add_action( "admin_bar_menu", array( &$this, "show_template_file_name_on_top" ), 9999 );
+		add_action( 'wp_enqueue_scripts', array( &$this, "add_current_template_stylesheet" ), 9999 );
 	}
 
-	/**
-	 * Show name.
-	 *
-	 * @global type $template
-	 * @global type $wp_admin_bar
-	 * @param type $wp_admin_bar admin bar.
-	 * @return type
-	 */
 	public function show_template_file_name_on_top( $wp_admin_bar ) {
 
 		if ( is_admin() or ! is_super_admin() ) {
@@ -70,15 +54,15 @@ class Show_Template_File_Name {
 		$template_relative_path	 = str_replace( ABSPATH . 'wp-content/', '', $template );
 
 		$current_theme		 = wp_get_theme();
-		$current_theme_name	 = $current_theme->name;
+		$current_theme_name	 = $current_theme->Name;
 		$parent_theme_name	 = '';
 
 		if ( is_child_theme() ) {
 			$child_theme_name	 = __( 'Theme name: ', 'show-current-template' )
 					. $current_theme_name;
-			$parent_theme_name	 = $current_theme->parent()->name;
+			$parent_theme_name	 = $current_theme->parent()->Name;
 			$parent_theme_name	 = ' (' . $parent_theme_name
-					. __( "'s child", 'show-current-template' ) . ')';
+					. __( "'s child", 'show-current-template' ) . ")";
 			$parent_or_child	 = $child_theme_name . $parent_theme_name;
 		} else {
 			$parent_or_child = __( 'Theme name: ', 'show-current-template' )
@@ -92,7 +76,7 @@ class Show_Template_File_Name {
 		foreach ( $included_files as $filename ) {
 			if ( strstr( $filename, 'themes' ) ) {
 				$filepath = strstr( $filename, 'themes' );
-				if ( $template_relative_path === $filepath ) {
+				if ( $template_relative_path == $filepath ) {
 					$included_files_list .= '';
 				} else {
 					$included_files_list .= '<li>' . "$filepath" . '</li>';
@@ -131,11 +115,7 @@ class Show_Template_File_Name {
 			. '</ul>',
 		) );
 	}
-	/**
-	 * Add style sheet.
-	 *
-	 * @return type
-	 */
+
 	public function add_current_template_stylesheet() {
 
 		if ( is_admin() or ! is_super_admin() ) {
@@ -149,7 +129,7 @@ class Show_Template_File_Name {
 		} else {
 			$is_older_than_3_8 = '-old';
 		}
-
+		
 		$stylesheet_path = plugins_url( 'css/style' . $is_older_than_3_8 . '.css', __FILE__ );
 		wp_register_style( 'current-template-style', $stylesheet_path );
 		wp_enqueue_style( 'current-template-style' );
