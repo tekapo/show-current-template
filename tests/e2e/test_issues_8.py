22d1b0a2d1d823e0f44412fb06269ed7f8f9801f
JOTAKI, Taisuke <tekapo@gmail.com>
2021-05-04 16:39:16 +0900

    cleanup some test codes.

--- tests/e2e/test_issues_8.py
+++ tests/e2e/test_issues_8.py
@@ -5,75 +5,10 @@
 #  open https://wp.plugin.sct.test/wp-admin/profile.php
 #  uncheck checkbox input#admin_bar_front
 #  open https://wp.plugin.sct.test/
-#  no js error occoured.
+#  no js error occurs.
 
 # So, to test this issue, uncheck the checkbox and confirm not output show-current-template/assets/js/replace.js
 
-
-# When you disable "Show Toolbar when viewing site" option, JavaScript error occurs.
-# https://wp.plugin.sct.test/wp-admin/profile.php
-
-# from playwright.sync_api import sync_playwright
-
-# def run(playwright):
-#     browser = playwright.chromium.launch(headless=False)
-#     context = browser.new_context()
-
-#     # Open new page
-#     page = context.new_page()
-
-#     # Go to https://wp.plugin.sct.test/wp-login.php?redirect_to=https%3A%2F%2Fwp.plugin.sct.test%2Fwp-admin%2Fprofile.php&reauth=1
-#     page.goto("https://wp.plugin.sct.test/wp-login.php?redirect_to=https%3A%2F%2Fwp.plugin.sct.test%2Fwp-admin%2Fprofile.php&reauth=1")
-
-#     # Click input[name="log"]
-#     page.click("input[name=\"log\"]")
-
-#     # Fill input[name="log"]
-#     page.fill("input[name=\"log\"]", "tai")
-
-#     # Press Tab
-#     page.press("input[name=\"log\"]", "Tab")
-
-#     # Fill input[name="pwd"]
-#     page.fill("input[name=\"pwd\"]", "tai")
-
-#     # Press Tab
-#     page.press("input[name=\"pwd\"]", "Tab")
-
-#     # Press Tab
-#     page.press("[aria-label=\"Show password\"]", "Tab")
-
-#     # Check input[name="rememberme"]
-#     page.check("input[name=\"rememberme\"]")
-
-#     # Press Tab
-#     page.press("input[name=\"rememberme\"]", "Tab")
-
-#     # Press Enter
-#     # with page.expect_navigation(url="https://wp.plugin.sct.test/wp-admin/profile.php"):
-#     with page.expect_navigation():
-#         page.press("text=Log In", "Enter")
-#     # assert page.url == "https://wp.plugin.sct.test/wp-admin/profile.php"
-
-#     # Uncheck input[name="admin_bar_front"]
-#     page.uncheck("input[name=\"admin_bar_front\"]")
-
-#     # Click input:has-text("Update Profile")
-#     # with page.expect_navigation(url="https://wp.plugin.sct.test/wp-admin/profile.php"):
-#     with page.expect_navigation():
-#         page.click("input:has-text(\"Update Profile\")")
-#     # assert page.url == "https://wp.plugin.sct.test/wp-admin/profile.php?updated=1"
-
-#     # Click text=Visit Site
-#     page.click("text=Visit Site")
-#     # assert page.url == "https://wp.plugin.sct.test/"
-
-#     # ---------------------
-#     context.close()
-#     browser.close()
-
-# with sync_playwright() as playwright:
-#     run(playwright)
 
 from playwright.sync_api import Page
 import pytest
@@ -82,7 +17,7 @@ import pytest
 @pytest.fixture()
 def wp_login(page: Page):
     """
-    docstring
+    Log in local WP admin.
     """
     page.goto("https://wp.plugin.sct.test/wp-login.php")
     page.fill('input[name="log"]', "tai")
@@ -94,7 +29,7 @@ def wp_login(page: Page):
 @pytest.fixture()
 def uncheck_toolbar(page: Page):
     """
-    docstring
+    uncheck input[name="admin_bar_front"]
     """
     page.goto("https://wp.plugin.sct.test/wp-admin/profile.php")
     page.fill('input[name="log"]', "tai")
@@ -108,7 +43,7 @@ def uncheck_toolbar(page: Page):
 @pytest.fixture()
 def check_toolbar(page: Page):
     """
-    docstring
+    check input[name="admin_bar_front"]
     """
     page.goto("https://wp.plugin.sct.test/wp-admin/profile.php")
     page.fill('input[name="log"]', "tai")
@@ -141,5 +76,4 @@ def test_output_replace_js(page: Page, check_toolbar):
     page.click("#wp-admin-bar-view-site a")
     jsSrc = page.get_attribute("#current-template-js-js", "src")
     substring = "show-current-template/assets/js/replace.js"
-    # print(jsSrc)
     assert substring in jsSrc
